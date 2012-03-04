4d758b7a83215ed95570e2b9fe3ad5958eb3fce4
Janne Heikkarainen <biotek@hushmail.com>
2012-03-04 17:27:38 +0200

    added gpl legal mumbo jumbo

--- threads.h
+++ threads.h
@@ -1,3 +1,24 @@
+/* dubstep */
+
+/*
+ *  (C) 2012 Janne Heikkarainen <janne.heikkarainen@tut.fi>
+ *
+ *  This file is part of Dubstep ANSI C Self-gravitating Smoothed Particle Hydrodynamics Simulator.
+ *
+ *  Dubstep is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Dubstep is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Dubstep.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __THREADSH__
 #define __THREADSH__
 
